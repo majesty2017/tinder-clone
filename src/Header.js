d5f4683571fbcdfb095f4d2823397a71a7713227
Majesty2017 <reborndzre@gmail.com>
2020-11-05 16:26:52 +0000

    first commit

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './Header.css'
+import PersonIcon from '@material-ui/icons/Person';
+import { IconButton } from '@material-ui/core';
 
 function Header() {
     return (
         <div className='header'>
-           <h2>I am a header</h2> 
+           <IconButton>
+                <PersonIcon fontSize='large' className='header__icon' />
+            </IconButton> 
+            <img 
+            className='header__logo' 
+            src='https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png'>
+
+            </img>
         </div>
     )
 }
